9629a50fab94b0f9a740628b436bb5f81d8f6aa7
Daniel Radosa <danysko5@gmail.com>
2023-03-28 08:09:48 +0200

    project tododoo

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header className="w-full h-12 bg-blue-500 flex items-center">
+      <h1 className="text-xl text-white text-center ml-4 mr-2 font-bold">Tododoo</h1>
+      <p className="text-md text-white/80">- Welcome to Tododoo.</p>
+    </header>
+  );
+};
+
+export default Header;
